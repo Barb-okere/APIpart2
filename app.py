cfead3bfb9318f43223f265da52d1655ec70e467
Barb <barbara.oluoch@strathmore.edu>
2024-11-28 00:57:37 +0300

    Memory storage for the products and request to create a new product

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 # Creates a flask app instance
+
+# In-memory storage for products
+products = []
+# This is a list that will store the products in memory
+
+# POST /products - Create a new product
+@app.route('/products', methods=['POST'])
+# This route will handle the POST request to create a new products
